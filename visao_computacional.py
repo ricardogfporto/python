e0bc2a50da89fba9b3c8bd729f611083273284bc
ricardogfporto <ricardo.porto@bakerhughes.com>
2023-08-03 08:57:28 -0300

    first commit

---
+++ visao_computacional.py
@@ -0,0 +1,13 @@
+import cv2 as cv 
+
+camera = cv.VideoCapture(0)
+rodando = True
+
+while rodando:
+
+    status, frame = camera.read()
+
+    if not status or cv.waitKey(1) & 0xff == ord('q'):
+        rodando = False
+
+    cv.imshow("Camera", frame)
